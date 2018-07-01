ce01f43fe5f7072e272dad8b0ca6ba856f3ea0ed
Neha Sharma <neha.sk@outlook.com>
2018-07-02 00:23:49 +0100

    dd

--- OnlineJwellery/App_Start/RouteConfig.cs
+++ OnlineJwellery/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace OnlineJwellery
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Account", action = "Register", id = UrlParameter.Optional }
+                defaults: new { controller = "Product", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
